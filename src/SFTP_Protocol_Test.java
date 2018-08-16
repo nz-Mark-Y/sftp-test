308501ac62642f1bd31323973ae63a5ffb29dfee
Mark Yep <ooksei@outlook.com>
2018-08-17 10:37:14 +1200

    changing port numbers

--- src/SFTP_Protocol_Test.java
+++ src/SFTP_Protocol_Test.java
@@ -2,7 +2,7 @@
 public class SFTP_Protocol_Test {
 
 	public static void main(String argv[]) {
-		int port = 118;
+		int port = 115;
 		
 		// Start server in its own thread
 		Thread serverThread = new Thread(){
